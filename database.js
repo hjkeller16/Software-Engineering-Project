76616a6dacc51291b803fde08e6a397124446490
Anna-Marie Baier <anna-marie.baier@live.de>
2018-10-17 11:35:38 +0200

    Add gitignore

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://hannah:hannah@localhost:5432/testdb';
-
+const connectionString = process.env.DATABASE_URL || `postgres://${config.postgres.user}:${config.postgres.password}@localhost:5432/testdb`;
 const client = new pg.Client(connectionString);
 client.connect();
+/* const query = client.query(
+  'CREATE TABLE mytest(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)'); */
 const query = client.query(
-  'CREATE TABLE mytest(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+  `INSERT INTO users VALUES ('Max', 'Mustermann')`);
 query.on('end', () => { client.end(); });
