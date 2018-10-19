acd199c0cc61ef39788140d1c1f7934e6b71df0a
Anna-Marie Baier <anna-marie.baier@live.de>
2018-10-19 21:45:16 +0200

    Add map

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -10,6 +10,21 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const L = window.L;
+    navigator.geolocation.getCurrentPosition(function (location) {
+      let latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+      let mymap = L.map('map').setView(latlng, 13);
+      mymap.on('load', () => {
+        document.getElementById('map-loading-indicator').style.display = 'none';
+      });
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=sk.eyJ1IjoiYW5uYWJhaWVyIiwiYSI6ImNqbmZ5OGczazA0dGczcHFubjh2MzhlNjcifQ.fgI9fzYtKDB3ZNNogLOyRA', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'sk.eyJ1IjoiYW5uYWJhaWVyIiwiYSI6ImNqbmZ5OGczazA0dGczcHFubjh2MzhlNjcifQ.fgI9fzYtKDB3ZNNogLOyRA'
+      }).addTo(mymap);
+      L.marker(latlng).addTo(mymap);
+    });
   }
 
 }
