b87654b46779882244c67de2fc6ec1525f203c83
Hannah Keller <hannahkeller@Hannahs-MacBook-Pro.local>
2018-12-05 19:38:10 +0100

    heroku test

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const comment = require('./comment');
 
 const app = express();
 
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cors());
 
@@ -57,4 +58,4 @@ if (process.argv[2] === 'debug') {
         '/', express.static(`${__dirname}/angular/dist`));
 }
 
-module.exports = app.listen(3000, () => console.log(`Server started.`));
+module.exports = app.listen(env.PORT || 3000, () => console.log(`Server started.`));
